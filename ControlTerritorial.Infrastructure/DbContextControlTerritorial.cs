2926caa17d21891e88d4a5a5a7182bfb538694ac
Juan Pablo Parrilla <juan.pablo.parrilla@nobu.com.ar>
2026-03-10 19:10:31 -0300

    Instalaciones de paquetes de EF, creacion de DbContext.

---
+++ ControlTerritorial.Infrastructure/DbContextControlTerritorial.cs
@@ -0,0 +1,16 @@
+﻿using ControlTerritorial.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControlTerritorial.Infrastructure
+{
+    public class DbContextControlTerritorial : DbContext
+    {
+        public DbContextControlTerritorial(DbContextOptions<DbContextControlTerritorial> options) : base(options)
+        {
+        }
+        public DbSet<Persona> Personas { get; set; }
+        public DbSet<Escuela> Escuelas { get; set; }
+        public DbSet<Mesa> Mesas { get; set; }
+        public DbSet<Vehiculo> Vehiculos { get; set; }
+    }
+}
--- ControlTerritorial/Program.cs
+++ ControlTerritorial/Program.cs
@@ -1,3 +1,6 @@
+using ControlTerritorial.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Cargar la configuración de appsettings.json y appsettings.{Environment}.json
+builder.Configuration
+       .SetBasePath(builder.Environment.ContentRootPath)
+       .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+       .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+
+builder.Services.AddDbContext<DbContextControlTerritorial>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
